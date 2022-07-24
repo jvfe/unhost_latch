2d6e172f1206e21e78b9e0a7e46644800095976f
jvfe <jvfecav@gmail.com>
2022-07-24 19:00:12 -0300

    feat: Add bowtie2 map task

--- wf/__init__.py
+++ wf/__init__.py
@@ -2,10 +2,13 @@ import subprocess
 from pathlib import Path
 from typing import List, Union
 
-from latch import cached_large_task, small_task, workflow
+from latch import large_task, small_task, workflow
+from latch.resources.tasks import cached_large_task
 from latch.types import LatchDir, LatchFile
 
 from .docs import UNHOST_DOCS
+
+CACHE_VERSION = "0.1.0"
 
 
 @small_task
@@ -45,7 +48,7 @@ def fastp(
     return LatchDir(str(output_dir), f"latch:///unhost_{sample_name}")
 
 
-@cached_large_task("0.1.0")
+@cached_large_task(CACHE_VERSION)
 def build_bowtie_index(
     host_genome: LatchFile, sample_name: str, host_name: str
 ) -> LatchDir:
@@ -67,6 +70,38 @@ def build_bowtie_index(
     return LatchDir(str(output_dir), f"latch:///unhost_{sample_name}_host_idx")
 
 
+#@cached_large_task(CACHE_VERSION)
+@large_task
+def map_to_host(
+    host_idx: LatchDir,
+    read_dir: LatchDir,
+    sample_name: str,
+    host_name: str,
+) -> LatchDir:
+
+    output_dir_name = f"{sample_name}_bt_unaligned"
+    output_dir = Path(output_dir_name).resolve()
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    _bt_cmd = [
+        "bowtie2/bowtie2",
+        "-x",
+        f"{host_idx.local_path}/{host_name}",
+        "-1",
+        f"{read_dir.local_path}/{sample_name}_1.trim.fastq.gz",
+        "-2",
+        f"{read_dir.local_path}/{sample_name}_2.trim.fastq.gz",
+        "--un-conc-gz",
+        f"{output_dir}/{sample_name}_unaligned.fastq.gz",
+        "--threads",
+        "31",
+    ]
+
+    subprocess.run(_bt_cmd)
+
+    return LatchDir(str(output_dir), f"latch:///unhost_{sample_name}_unaligned")
+
+
 @workflow(UNHOST_DOCS)
 def unhost(
     read1: LatchFile,
@@ -83,9 +118,17 @@ def unhost(
     A fastq preprocessing and host read removal workflow
     for short-read metagenomics data.
     """
+
     trimmed_data = fastp(read1=read1, read2=read2, sample_name=sample_name)
     host_idx = build_bowtie_index(
         host_genome=host_genome, sample_name=sample_name, host_name=host_name
     )
 
-    return [trimmed_data, host_idx]
+    unaligned = map_to_host(
+        host_idx=host_idx,
+        read_dir=trimmed_data,
+        sample_name=sample_name,
+        host_name=host_name,
+    )
+
+    return [unaligned]
